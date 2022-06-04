d43c18b86fd9ac9764b61d88e3f68e1605c72102
Febrian Dani Ritonga <87335182+daniertg@users.noreply.github.com>
2022-06-04 08:45:33 +0700

    Update node_12.java

--- node_12.java
+++ node_12.java
@@ -1,12 +1,12 @@
 public class node_12 {
     Pembeli pembeli;
-    Pesanan pesanan;
+    Pesan pesan;
     node_12 prev, next;
 
-    node_12(node_12 prev, Pembeli Pembeli, Pesanan pesanan, node_12 next){
+    node_12(node_12 prev, Pembeli Pembeli, Pesan pesan, node_12 next){
         this.prev=prev;
         this.pembeli=pembeli;
-        this.pesanan=pesanan;
+        this.pesan=pesan;
         this.next=next;
     }
 }
