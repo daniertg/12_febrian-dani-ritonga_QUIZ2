6381e3a4d6da6bd79c2bac7ca1a00ed74de4731c
Febrian Dani Ritonga <87335182+daniertg@users.noreply.github.com>
2022-06-04 08:44:52 +0700

    Create Pesanan.java

---
+++ Pesanan.java
@@ -0,0 +1,11 @@
+public class Pesan {
+    int kodePesan, harga, jumlahPesan;
+    String namaPesan;
+
+    Pesan(int kode, String nama, int harga, int jumlahPesan{
+        this.kodePesan = kode;
+        this.namaPesan = nama;
+        this.harga = harga;
+        this.jumlahPesan;
+    }
+}
