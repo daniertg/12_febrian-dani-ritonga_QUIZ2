4ee04c059fa8baaa63b4877e3303e31727d6503f
Febrian Dani Ritonga <87335182+daniertg@users.noreply.github.com>
2022-06-04 09:49:04 +0700

    Update menuMain.java

--- menuMain.java
+++ menuMain.java
@@ -1,1 +1,49 @@
+public class node_12 {
+    static String pilih,pilih2;
+    static boolean succes = true;
 
+    
+        public static void menu()
+        {
+            while (succes) {
+                System.out.println("================================================");
+                System.out.println("Menu");
+                System.out.println("1. Tambah antrian");
+                System.out.println("2. Cetak antrian");
+                System.out.println("3. Hapus Antrian");
+                System.out.println("4. Laporan pengurutan by dani");
+                System.out.println("5. Hitung total pendapatan");
+                System.out.println("6. Keluar");
+                System.out.print("pilih:(1-6)");
+                pilih2 = sc.nextLine();
+              
+              
+             public static void main(String[] args) throws Exception {
+        DoubleLinkedLists dll = new DoubleLinkedLists();
+        Scanner sc = new Scanner(System.in);
+        int nomorAntrian = 0;
+        int pilih;
+        String[] nama = {"aren", "aran", "irin", "oron"};
+        String[] nohp  = {"081111111", "08222222", "08333333", "084444"};
+        int[] kodePesanan = {1,2,3,4};
+        String[] namaPesanan = {"bakso", "mie ", "Nasi goreng", "burger"};
+        int[] harga = {15000, 25000, 10000, 15000};
+        
+       public static void main(String[] args) {
+               
+                if (pilih2.equals("1")) {
+                     Pembeli pem = new Pembeli(nama[nomorAntrian], nohp[nomorAntrian], kodePesanan[nomorAntrian]);
+                    Pesanan pes = new Pesanan(kodePesanan[nomorAntrian], harga[nomorAntrian], namaPesanan[nomorAntrian]);
+                    dll.addLast(pem, pes);
+                    System.out.println("Nomor Antrian : " + nomorAntrian+1);
+                    System.out.println("Nama Customer : " + nama[nomorAntrian]);
+                    System.out.println("Nomor Hp : " + nohp[nomorAntrian]);
+                    nomorAntrian++;
+                } else if (pilih2.equals("2")) {
+                    
+                } else if (pilih2.equals("3")) {
+                    ();
+                } else if (pilih2.equals("x")) {
+                    succes = false;
+                }
+            }
